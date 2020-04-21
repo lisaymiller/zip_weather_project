3279b4198d9bd4beca189d21b4675392f3e0b96e
Lisa Miller <lisayevonne@outlook.com>
2020-04-21 14:00:25 -0400

    final push

--- index.js
+++ index.js
@@ -31,30 +31,25 @@ var formInput = $("#target").val();
 // Function when user clicks button 
 btn.onclick = function() {
   modal.style.display = "block";
-  // $("#city").text('city replacement');
-  // $("#weather").text('weather replacement');
-  // $("#dateTime").text('date replacement');
-  $("#city").text($("inputBox"));
-  return 
+  let userInput = $("#inputBox").val();
+  return currentWeather(userInput);
 }
-
-
 
 //Function when user presses enter key
 $('input').on('keyup', function (e) {                
                 if(e.which === 13){
                  	let modalFunc = function() {
-              		modal.style.display = "block";
-                  $("#city").text($("inputBox"));
-                  // $("#weather").text('weather replacement');
-                  // $("#dateTime").text('date replacement');
+                  modal.style.display = "block";
+                  let userInput = $("#inputBox").val();
+                  // $("#city").text();
+                  // $("#weather").text();
+                  // $("#dateTime").text();
+                  return currentWeather(userInput);
             		}
                 	return modalFunc();
                 }
             });
     
-    // Form will get the user's zipcode when submitted
-
 
 function currentWeather(zipcode) {
     // OpenWeather API key
@@ -67,48 +62,17 @@ function currentWeather(zipcode) {
         .then(function(weather){
 
         // Add weather objects
-        $("#city").text("City: " + weather.name);
-        $("#weather").text("Description: " + weather.weather[0].description);
+        $("#city").text(weather.name);
+        $("#weather").text("looking like " + weather.weather[0].description + ' for right now');
 
-        // moment is now working! - working on having it display the user's local timezone using getTimezoneOffset()
+        // Moment -- date and time
         let timeZone = weather.timeZone;
         let time = moment.tz(timeZone).utcOffset('-0400').format('dddd') + ', ' + moment.tz(timeZone).utcOffset('-0400').format('hh:mm a');
-        $(".dateTime").text("Time: " + time);
-
-        // Meters to Miles - this is for the wind speed display
-        function milesConvert(meters){
-            // calculate meters to miles and display it as a whole number
-            var miles = meters * (25 / 11);
-            $(".wind").text("Wind: " + Math.round(meters) + "mph");
-        };
-
-        // Kelvin to Fahrenheit
-        function fahrenheitConvert(kelvin) {
-            // calculate kelvin to fahrenheit and display it as a whole number
-            var fahrenheit = (kelvin - 273.15) * 1.80 + 32;
-            $(".temperature").text("Temperature: " + Math.round(fahrenheit) + "°F");
-        };
-
-
-        // couldn't get moment to work - it kept throwing my alert box up.. attempting to fix
-        // var moment = require('moment');
-        // function currentTime(time){
-        //     var time = new Moment();
-        //     var timeZone = weather.timezone;
-        //     return moment().tz(timeZone).format('hh:mm a');
-        // }
-
-        // conversion functions
-        milesConvert(weather.wind.speed); 
-        fahrenheitConvert(weather.main.temp); 
-        wayTheWindBlows(weather.wind.deg);
-        // currentTime(moment);
-
-        $("#current-conditions").show(2000);
-        console.log(weather);
+        $("#dateTime").text(time);
 
     }).catch(function(){
-        alert("Please enter a valid zip code.")
-        console.log("User needs to enter a zip code.")
+        if (($("#inputBox").val()) === '')
+        alert("Please enter a zip code to get started.")
+        return false;
     });
 }
